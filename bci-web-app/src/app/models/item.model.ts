e1d392e23480d96dd24cbf3be8cb1a4c638a8cec
Chad Bernstein <chaddbernstein@gmail.com>
2024-05-16 12:32:00 -0400

    Revert "Initial Commit"
    This reverts commit 313cf2070e0b1c3f988b684dd8c6002a02cc2073.

--- bci-web-app/src/app/models/item.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Item {
-    id: number;
-    title: string;   
-    description: string;   
-    category: string;   
-    imageUrl: string;  
-    price: number;
-    quantity: number;
-}
